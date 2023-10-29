4c455da61ec4f89e0cefbbc8c857e4135f332e9e
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-29 10:22:20 -0400

    profile div done in js

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@ export async function getUser(username){
     const response = await fetch(v.apiURL + username);
     const data = await response.json();
 
-    // console.log(data);
+    console.log(data);
     if (!response.ok){
         errorMessage("User not found, try another");
     } else {
@@ -18,4 +18,22 @@ export function errorMessage(msg){
     v.profile.innerHTML = "";
     document.querySelector(".hide").style.display = "none";
     return v.repos.innerHTML = `<p class="alert alert-danger">${msg}</p>`;
+}
+
+
+// Display data
+export function displayData(user){
+    const html = `
+        <img src="${user.avatar_url}" alt="${user.name}" class="img-thumbnail rounded-circle" />
+        <h2>${user.name}</h2>
+        <p>${user.login}</p>
+        <div class="d-grid">
+            <a href="https://github.com/${user.login}" target="_blank" rel="noopener" class="btn btn-outline-secondary">View Profile</a>
+        </div>  
+        <p class="pt-2"><span>${user.followers} Followers</span>   
+                             <span>  ${user.following} Following</span></p>
+        <p>${user.public_repos} Public repos</p>
+        <p><i class="fas fa-marker-alt"></i>${user.location}</p>
+        `;
+        v.profile.innerHTML = html;
 }
