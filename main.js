cd10219a13948ab618f5e2b191b36d729e34803f
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-29 09:46:07 -0400

    empty and invalid username

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import * as v from "./js/variables.js";
+import { getUser, errorMessage } from "./js/functions.js"
 
 v.form.addEventListener("submit", (e) => {
     e.preventDefault();
     // let user = v.search.value;
-    let user = v.search.value.split(" ").join("");//remove the empty space between
     // let user = v.search.value.replace(/\s+/g, "");//another way to remove the empty space between
+    let user = v.search.value.split(" ").join("");//remove the empty space between
     
-
     if (user === ""){
-        v.errorMessage("Input cannot be empty");
+        errorMessage("Input cannot be blank");
+        console.log("blank");
     }
     else {
         getUser(user);
